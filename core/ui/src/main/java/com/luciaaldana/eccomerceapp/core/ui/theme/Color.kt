77e73d100bfb45a32c37f5c3ef26cdcd20278f92
luciaaldana <luciaaldanacastillo@gmail.com>
2025-07-15 20:06:21 -0300

    feat: add theme and toogle theme

--- core/ui/src/main/java/com/luciaaldana/eccomerceapp/core/ui/theme/Color.kt
+++ core/ui/src/main/java/com/luciaaldana/eccomerceapp/core/ui/theme/Color.kt
@@ -2,65 +2,10 @@ package com.luciaaldana.eccomerceapp.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Primary color oklch(55.3% 0.195 38.402) converted to hex
-val Primary = Color(0xFF8B5A2B)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
 
-// Light theme colors
-val LightPrimary = Color(0xFF8B5A2B)
-val LightOnPrimary = Color(0xFFFFFFFF)
-val LightPrimaryContainer = Color(0xFFFFDCC7)
-val LightOnPrimaryContainer = Color(0xFF2F1500)
-
-val LightSecondary = Color(0xFF765847)
-val LightOnSecondary = Color(0xFFFFFFFF)
-val LightSecondaryContainer = Color(0xFFFFDCC7)
-val LightOnSecondaryContainer = Color(0xFF2B1709)
-
-val LightTertiary = Color(0xFF646032)
-val LightOnTertiary = Color(0xFFFFFFFF)
-val LightTertiaryContainer = Color(0xFFEBE4AB)
-val LightOnTertiaryContainer = Color(0xFF1F1D00)
-
-val LightError = Color(0xFFBA1A1A)
-val LightOnError = Color(0xFFFFFFFF)
-val LightErrorContainer = Color(0xFFFFDAD6)
-val LightOnErrorContainer = Color(0xFF410002)
-
-val LightBackground = Color(0xFFFFFBF7)
-val LightOnBackground = Color(0xFF201A16)
-val LightSurface = Color(0xFFFFFBF7)
-val LightOnSurface = Color(0xFF201A16)
-val LightSurfaceVariant = Color(0xFFF2DFD1)
-val LightOnSurfaceVariant = Color(0xFF51453A)
-val LightOutline = Color(0xFF837569)
-val LightOutlineVariant = Color(0xFFD5C3B5)
-
-// Dark theme colors
-val DarkPrimary = Color(0xFFFFB786)
-val DarkOnPrimary = Color(0xFF4A2800)
-val DarkPrimaryContainer = Color(0xFF693E15)
-val DarkOnPrimaryContainer = Color(0xFFFFDCC7)
-
-val DarkSecondary = Color(0xFFE4BFA6)
-val DarkOnSecondary = Color(0xFF432B1C)
-val DarkSecondaryContainer = Color(0xFF5C4131)
-val DarkOnSecondaryContainer = Color(0xFFFFDCC7)
-
-val DarkTertiary = Color(0xFFCFC890)
-val DarkOnTertiary = Color(0xFF343208)
-val DarkTertiaryContainer = Color(0xFF4B481D)
-val DarkOnTertiaryContainer = Color(0xFFEBE4AB)
-
-val DarkError = Color(0xFFFFB4AB)
-val DarkOnError = Color(0xFF690005)
-val DarkErrorContainer = Color(0xFF93000A)
-val DarkOnErrorContainer = Color(0xFFFFDAD6)
-
-val DarkBackground = Color(0xFF17120E)
-val DarkOnBackground = Color(0xFFEDE0DB)
-val DarkSurface = Color(0xFF17120E)
-val DarkOnSurface = Color(0xFFEDE0DB)
-val DarkSurfaceVariant = Color(0xFF51453A)
-val DarkOnSurfaceVariant = Color(0xFFD5C3B5)
-val DarkOutline = Color(0xFF9E8F82)
-val DarkOutlineVariant = Color(0xFF51453A)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
